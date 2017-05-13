b0f135bdf6d604d84d14aa57e78081f1896baa67
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2017-05-13 15:35:54 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from distutils.core import setup
 
+setup(
+    name = 'textpack',
+    packages = ['textpack'],
+    version = '1.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'test in progress . . .',
+    author = 'abdur-rahmaanj',
+    author_email = 'arj.python@gmail.com',
+    url = 'https://github.com/Abdur-rahmaanJ/test_pack',
+    download_url = 'https://github.com/Abdur-rahmaanJ/test_pack/archive/1.0.zip',
+    keywords = ['test', 'trial'],
+    classifiers = [],
+)
